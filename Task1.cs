d8c6a41d1afa4e6c9656be1c05d854cc258ebf93
Kirill Sachkov <playvirtul@gmail.com>
2022-07-16 17:56:00 +0300

    first commit

---
+++ Task1.cs
@@ -0,0 +1,23 @@
+﻿namespace Program
+{
+    public class Task1
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static void ReplaceFirstWay(ref int X, ref int Y, ref int Z)
+        {
+            (X, Y, Z) = (Y, Z, X);
+        }
+
+        public static void ReplaceSecondWay(ref int X, ref int Y, ref int Z)
+        {
+            Y = X + Y + Z;
+
+            X = Y - (X + Z);
+            Z = Y - (X + Z);
+            Y = Y - (X + Z);
+        }
+    }
+}
